f029922fe0eba1c0d358e352e60b85e67d656462
perezlx <perezlx@HGDLAPPEREZLX>
2019-03-21 11:31:00 -0600

    Add project files.

---
+++ ValidacionCadena/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacionCadena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            String s = "";
+            char[] c = new char[] { '#', '@', '}' };
+
+            Console.WriteLine("1) alphabetic \n 2) alphanumeric \n 3) numeric: ");
+            i = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Linea: ");
+            s = Console.ReadLine();
+
+            switch(i)
+            {
+                case 1:
+                    s.ValidateAlphabeticString(c);
+                    break;
+                case 2:
+                    s.ValidateAlphanumericString(c);
+                    break;
+                case 3:
+                    s.ValidateNumericString(c);
+                    break;
+            }
+
+        }
+    }
+}
---
+++ ValidacionCadena/metodos.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacionCadena
+{
+    public static class Metodos
+    {
+        private static bool ValidateString(String str, char[] specialChar, Func<char, bool> fn, bool acceptSpace =  true)
+        {
+            List<char> tempStr = str.ToList();
+            bool result = true;
+
+            if(acceptSpace)
+                tempStr.RemoveAll(c => c == ' ');
+
+            if (specialChar != null)
+                tempStr.RemoveAll(c => specialChar.Contains(c));
+
+            result = tempStr.All(c => fn(c));
+
+            if(result == false)
+            {
+                Console.WriteLine("¡Caracter invalido detectado!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("¡Valido!");
+                Console.ReadKey();
+            }
+
+            return result;
+        }
+
+        public static bool ValidateAlphabeticString(this String str, char[] specialChar, bool acceptSpace = true)
+        {
+            return ValidateString(str, specialChar, char.IsLetter, acceptSpace);
+        }
+
+        public static bool ValidateAlphanumericString(this String str, char[] specialChar, bool acceptSpace = true)
+        {
+            return ValidateString(str, specialChar, char.IsLetterOrDigit, acceptSpace);
+        }
+
+        public static bool ValidateNumericString(this String str, char[] specialChar, bool acceptSpace = true)
+        {
+            return ValidateString(str, specialChar, char.IsNumber, acceptSpace);
+        }
+    }
+}
